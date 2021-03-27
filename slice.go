f719a32467ef5678a53fc12f78ea0450eabb8fdf
rakesh <rakesh.ss@target.com>
2021-03-27 22:02:11 +0530

    iterate the slice

--- slice.go
+++ slice.go
@@ -7,6 +7,11 @@ func main(){
 	cards := []string {"one","two"}
 	cards = append(cards,newCard())
 	fmt.Println(cards)
+
+	// loop the slice
+	for i,card := range(cards){
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string{
