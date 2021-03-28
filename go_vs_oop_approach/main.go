6c292fb7ebdbe32df8264c172d7efcf48dcc07b8
rakesh <rakesh.ss@target.com>
2021-03-28 12:20:43 +0530

    go vs oop approach examples

---
+++ go_vs_oop_approach/main.go
@@ -0,0 +1,19 @@
+package main
+
+import("fmt")
+
+func main(){
+
+	cards := deck {"one","two"}
+	cards = append(cards,newCard())
+	fmt.Println(cards)
+
+	// loop the slice
+	for i,card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string{
+	return "Five of Diamonds"
+}
